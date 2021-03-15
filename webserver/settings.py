f5510556b75cb993add68d48bf183b185bdd83ae
leonardo.giuliana <leonardo.giuliana@gmail.com>
2021-03-15 22:55:36 +0100

    ** settings.ws **

--- webserver/settings.py
+++ webserver/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o*3fsd&muop8v-n&3w^^(x26xu3ukwr^lxiebh2kaym4*uhve!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
